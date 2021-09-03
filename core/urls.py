1e033dd59b93ccfb9f736648768d8dca72fe1a9c
Ruslan Hamidullin <ruslan.hamidullin@starnavi.io>
2021-09-03 10:16:25 +0300

    Add User Permission delete endpoint

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('users/', views.UserListView.as_view(), name='core-user_list'),
     path('users/<int:user_id>/', views.UserDetailView.as_view(), name='core-user_detail'),
     path('users/<int:user_id>/permissions/', views.UserPermissionsListView.as_view(), name='core-user_permissions_list'),
+    path('users/<int:user_id>/permissions/<int:permission_id>/', views.UserPermissionsDetailView.as_view(), name='core-user_permission_detail'),
 ]
--- core/views.py
+++ core/views.py
@@ -49,31 +49,26 @@ class UserPermissionsListView(generics.ListCreateAPIView):
             user = self.get_current_user()
             user.user_permissions.add(permission)
         else:
-            raise NotFound(detail="Not Found.", code=None)
+            raise NotFound(detail="Not Found.")
 
 
-# class UserPermissionsDetailView(generics.RetrieveAPIView,
-#                                 generics.DestroyAPIView):
-#     """
-#     Retrieve user permission and delete them
-#     """
-#     serializer_class = PermissionSerializer
-#     lookup_url_kwarg = "permission_id"
-#     user_lookup_url_kwarg = "user_id"
-#
-#     def get_current_user(self):
-#         user_lookup_url_kwarg = self.kwargs.get(self.user_lookup_url_kwarg)
-#         return get_object_or_404(User, id=user_lookup_url_kwarg)
-#
-#     def get_queryset(self):
-#         user = self.get_current_user()
-#         return user.user_permissions.all()
-#
-#     def retrieve(self, request, *args, **kwargs):
-#         instance = self.get_object()
-#         serializer = self.get_serializer(instance)
-#         return Response(serializer.data)
-#
-#     def perform_destroy(self, permission):
-#         user = self.get_current_user()
-#         user.user_permissions.remove(permission)
+class UserPermissionsDetailView(generics.RetrieveAPIView,
+                                generics.DestroyAPIView):
+    """
+    Retrieve user permission and delete them
+    """
+    serializer_class = PermissionSerializer
+    lookup_url_kwarg = "permission_id"
+    user_lookup = "user_id"
+
+    def get_current_user(self):
+        user_lookup_url_kwarg = self.kwargs.get(self.user_lookup)
+        return get_object_or_404(User, id=user_lookup_url_kwarg)
+
+    def get_queryset(self):
+        user = self.get_current_user()
+        return user.user_permissions.all()
+
+    def perform_destroy(self, permission):
+        user = self.get_current_user()
+        user.user_permissions.remove(permission)
